f86ac961eb60c0778e75c0daf75294906c0478df
mpjr1 <mapg.jr@gmail.com>
2015-06-26 14:57:03 +0000

    Assignment 32 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,39 @@
 
 require 'faker'
 
-# Create Posts
-50.times do
-  Post.create!(
-    title: Faker::Lorem.sentence,
-    body:  Faker::Lorem.paragraph
-    )
-end
-posts = Post.all
+# # Create Posts
+# 50.times do
+#   Post.create!(
+#     title: Faker::Lorem.sentence,
+#     body:  Faker::Lorem.paragraph
+#     )
+# end
+# posts = Post.all
 
-# Create Comments
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
-    )
+# # Create Comments
+# 100.times do
+#   Comment.create!(
+#     post: posts.sample,
+#     body: Faker::Lorem.paragraph
+#     )
+# end
+
+#Verify the Post doesn't exist  in the data base yet.
+if Post.where(title: "Unique First Post").blank? 
+# Create Unique Post
+Post.create!(
+  title: "Unique First Post",
+  body: "Unique First Body Text")
 end
+
+#Verify Second Post doesn't exist  in the data base yet.
+if Post.where(title: "Unique Second Post").blank? 
+# Create Unique Post
+Post.create!(
+  title: "Unique Second Post",
+  body: "Unique Second Body Text")
+end
+
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
