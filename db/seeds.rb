a592c3005fec7e031d2cc64368965fa18cbcf136
mpjr1 <mapg.jr@gmail.com>
2015-06-27 18:37:27 +0000

    Assigment 33 Round 2 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,33 @@
 
 require 'faker'
 
-# Create Posts
-50.times do
-  Post.create!(
-    title: Faker::Lorem.sentence,
-    body:  Faker::Lorem.paragraph
-    )
-end
-posts = Post.all
+# # Create Posts
+# 50.times do
+#   Post.create!(
+#     title: Faker::Lorem.sentence,
+#     body:  Faker::Lorem.paragraph
+#     )
+# end
+# posts = Post.all
 
-# Create Comments
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
+# # Create Comments
+# 100.times do
+#   Comment.create!(
+#     post: posts.sample,
+#     body: Faker::Lorem.paragraph
+#     )
+# end
+
+# Create Advertisements
+50.times do
+  Advertisement.create!(
+    title: Faker::Lorem.sentence,
+    copy: Faker::Lorem.paragraph,
+    price: Faker::Number.number(2)
     )
 end
 
 puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comment created"
+#puts "#{Post.count} posts created"
+#puts "#{Comment.count} comment created"
+puts "#{Advertisement.count} Advertisements created"
