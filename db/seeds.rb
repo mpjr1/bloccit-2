96ab04e5d0a30a4a36c24e8c328be534b1dcddf5
mpjr1 <mapg.jr@gmail.com>
2015-06-28 02:30:16 +0000

    Assignment 35 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,33 @@
 
 require 'faker'
 
-# Create Posts
-50.times do
-  Post.create!(
-    title: Faker::Lorem.sentence,
-    body:  Faker::Lorem.paragraph
-    )
-end
-posts = Post.all
+# # Create Posts
+# 50.times do
+#   Post.create!(
+#     title: Faker::Lorem.sentence,
+#     body:  Faker::Lorem.paragraph
+#     )
+# end
+# posts = Post.all
 
-# Create Comments
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
+# # Create Comments
+# 100.times do
+#   Comment.create!(
+#     post: posts.sample,
+#     body: Faker::Lorem.paragraph
+#     )
+# end
+
+# Create Questions
+35.times do 
+  Question.create!(
+    title: Faker::Lorem.sentence,
+    body:  Faker::Lorem.paragraph,
+    resolved: false
     )
 end
 
 puts "Seed finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comment created"
+# puts "#{Post.count} posts created"
+# puts "#{Comment.count} comment created"
+puts "#{Question.count} questions created"
