27ecca80774abc38a8ab40ab1acb51f0c00e9afd
mpjr1 <mapg.jr@gmail.com>
2015-07-22 20:41:24 +0000

    Checkpoint 49 voting completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,16 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:update]
 
- resources :topics do
+  resources :topics do
     resources :posts, except: [:index]
- end
+  end
    
   resources :posts, only: [] do  
     resources :comments, exept: [:index]
-    end
+  
+  post '/up-vote' => 'votes#up_vote', as: :up_vote
+  post '/down-vote' => 'votes#down_vote', as: :down_vote
+  end
   
   get 'about' => 'welcome#about'
   root to: 'welcome#index'
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,8 +1,22 @@
-describe Vote do
-  describe "validations" do
+require 'rails_helper'
+
+
+RSpec.describe Vote, type: :model do
+ describe "validations" do
+   
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        expect(value).to eq (-1 || 1)
+        first_test_value = Vote.new(value: 1)
+        expect(first_test_value.valid?).to eq(true)
+        
+        second_test_value = Vote.new(value: -1)
+        expect(second_test_value.valid?).to eq(true)
+        
+        third_test_value = Vote.new(value: 2)
+        expect(third_test_value.valid?).to eq(false)
+        
+        fourth_test_value = Vote.new(value: -3)
+        expect(fourth_test_value.valid?).to eq(false)
       end
     end
   end
