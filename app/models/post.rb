d4f89d7bf71cd1b639ef8719426e694375f5beb3
mpjr1 <mapg.jr@gmail.com>
2015-07-23 13:37:01 +0000

    Complete voting assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,13 @@ class Post < ActiveRecord::Base
   belongs_to :topic
   mount_uploader :image, ImageUploader
   
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  #validates :topic, presence: true
+  #validates :user, presence: true
+  
+  default_scope { order('rank DESC') }
+  
   def up_votes
     votes.where(value: 1).count
   end
@@ -16,18 +23,20 @@ class Post < ActiveRecord::Base
   def points
     up_votes - down_votes
   end
-  
-  default_scope { order('rank DESC') }
-  
-   validates :title, length: { minimum: 5 }, presence: true
-   validates :body, length: { minimum: 20 }, presence: true
-   #validates :topic, presence: true
-   #validates :user, presence: true
-  
+    
   def update_rank
      age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
      new_rank = points + age_in_days
  
      update_attribute(:rank, new_rank)
    end
+
+  after_create :create_vote
+  
+  private
+  
+  def create_vote
+    user.votes.create(value: 1, post: self)
+  end
+    
 end
