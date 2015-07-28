c913ab72b374b5dc72859fa7082dc2cfe582c470
mpjr1 <mapg.jr@gmail.com>
2015-07-28 14:36:59 +0000

    Checkpoint 50 completed, Mocking with RSpec

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,8 +7,8 @@ class Post < ActiveRecord::Base
   
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  #validates :topic, presence: true
-  #validates :user, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
   
   default_scope { order('rank DESC') }
   
@@ -30,11 +30,7 @@ class Post < ActiveRecord::Base
  
      update_attribute(:rank, new_rank)
    end
-
-  after_create :create_vote
-  
-  private
-  
+ 
   def create_vote
     user.votes.create(value: 1, post: self)
   end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
+include TestFactories
 
 RSpec.describe Vote, type: :model do
  describe "validations" do
@@ -20,4 +21,13 @@ RSpec.describe Vote, type: :model do
       end
     end
   end
+  
+  describe 'after_save' do
+     it "calls `Post#update_rank` after save" do
+       post = associated_post
+       vote = Vote.new(value: 1, post: post)
+       expect(post).to receive(:update_rank)
+       vote.save
+     end
+   end
 end
