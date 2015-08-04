34d90f4bdf611f7f47eb4b5c58e162251e6e42f7
mpjr1 <mapg.jr@gmail.com>
2015-08-04 16:36:34 +0000

    Assignment 52 completed

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,6 @@ class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
   
   scope :visible_to, -> (user) { user ? all : where(public: true) }
+  scope :publicly_visible, -> {user ? all : where(public: true) }
+  scope :private_visible, -> {user ? all : where(public:false) }
 end
