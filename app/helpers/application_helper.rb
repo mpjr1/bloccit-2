091b7a52955ff4436b4fcad34b83f2e8d494573e
mpjr1 <mapg.jr@gmail.com>
2015-08-05 16:40:39 +0000

    Assignment 53 completed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,12 @@ module ApplicationHelper
     (redcarpet.render markdown).html_safe
   end
   
+  def up_vote_link_classes(post)
+  "glyphicon glyphicon-chevron-down #{(current_user.voted(post) && current_user.voted(post).up_vote?) ? 'voted' : '' }"
+  end
+    
+  def down_vote_link_classes(post)
+    "glyphicon glyphicon-chevron-down #{(current_user.voted(post) && current_user.voted(post).down_vote?) ? 'voted' : '' }"
+  end
+  
 end
