15da5cd2ceaad95ac5f6a31855a7f0d572e1dc36
mpjr1 <mapg.jr@gmail.com>
2015-09-03 15:39:50 +0000

    Changed URL pointing to

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,7 @@ blocmetrics.report = function(eventName) {
 
    var event = { name: eventName };
    var request = new XMLHttpRequest();
-   request.open("POST", "http://localhost:3000/api/events", true);
+   request.open("POST", "http://mpjr1-blocmetrics.herokuapp.com/api/events", true);
    request.setRequestHeader('Content-Type', 'application/json');
    request.send(JSON.stringify(event));
 }
